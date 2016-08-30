586d8d5ff311c122ff8bb9d9f7966c98379ed3f9
James Mitchell <say.wibble@gmail.com>
2016-08-30 19:55:47 +1200

    Refactor the older auth code to a widget/component version.

--- app/scripts/mymovie/app.module.js
+++ app/scripts/mymovie/app.module.js
@@ -1,23 +1,19 @@
 // mymovie app
 var angular = require('angular');
 require('angular-ui-router');
-require('satellizer');
 
 var config = require('./app.config');
 var routes = require('./app.states');
-var appController = require('./app.controller');
-var loginController = require('./auth/login.controller');
+var auth = require('./auth');
 var search = require('./search');
 
 var app = angular.module('app', [
     'ui.router',
-    'satellizer',
-    search
+    search,
+    auth
 ]);
 
 app.config(config);
 app.config(routes);
-app.controller('AppController', appController);
-app.controller('LoginController', loginController);
 
 module.exports = app;
--- app/scripts/mymovie/auth/login.controller.js
+++ app/scripts/mymovie/auth/login.controller.js
@@ -1,6 +1,22 @@
-module.exports = /* @ngInject */ function LoginController($auth) {
-  var vm = this;
-  vm.authenticate = function(provider) {
-    $auth.authenticate(provider);
-  };
-};
+var angular = require('angular');
+require('satellizer');
+
+var app = angular.module('auth', ['satellizer']);
+
+app.component('loginGoogle', {
+  templateUrl: 'views/partial/loginController.html',
+  controller: 'LoginGoogleController'
+});
+
+app.controller('LoginGoogleController', [
+  '$auth',
+  function($auth) {
+    var ctrl = this;
+    ctrl.authenticate = function (provider) {
+      $auth.authenticate(provider);
+    };
+    return ctrl;
+  }
+]);
+
+module.exports = app;
